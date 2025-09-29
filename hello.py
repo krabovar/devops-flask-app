d10630373bc25db597b78d910b69374e5da01d77
krabovar <mark2003.ua@gmail.com>
2025-09-29 18:52:28 +0000

    changed to welcome

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def say_hello():
 	return(
-		 '<p><a href="/about">Hello, World, I am a Flask app! Click on me!</a></p>'
+		 '<p><a href="/about">Welcome!</a></p>'
 	       	'<p><a href="/contact">My Email is here!</a></p>'
 )
 @app.route('/about')
